2f3ae9da8ac2c12faf3a509dec13dabaec5c0475
victor costantini <167801447+vittoriocostantini@users.noreply.github.com>
2024-10-13 17:22:24 -0400

    Añadida componente Input

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
-import { IonContent } from '@ionic/react';
+import { IonContent, IonFooter, IonToolbar, IonIcon } from '@ionic/react';
+import { send } from 'ionicons/icons';
+import '../theme/ChatPage.css';
 
 function ChatPage({ nav }: { nav: HTMLIonNavElement }) {
   return (
-    <IonContent>
-      <h1>Chat para z</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
-      <h2>Hola</h2>
-    </IonContent>
+    <>
+      <IonContent>
+        <h1>Chat para z</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+        <h2>Hola</h2>
+      </IonContent>
+      <IonFooter>
+        <IonToolbar>
+          <div className="input-container">
+            <input 
+              className="ion-chat-input-message"
+            />
+            <IonIcon 
+              icon={send} 
+                onClick={() => {
+                // Lógica para manejar el clic en el icono
+              }}
+              className="ion-chat-icon"
+            />
+          </div>
+        </IonToolbar>
+      </IonFooter>
+    </>
   );
 };
 
