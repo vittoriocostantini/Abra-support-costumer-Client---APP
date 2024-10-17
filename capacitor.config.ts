31e8e95048ef18d6aabc1336cf950e4f08b05905
victor costantini <167801447+vittoriocostantini@users.noreply.github.com>
2024-10-17 08:55:31 -0400

    Instalar Plugin Capacitor@Keyboard

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,18 @@
 import type { CapacitorConfig } from '@capacitor/cli';
+import { KeyboardResize, KeyboardStyle } from '@capacitor/keyboard';
+
 
 const config: CapacitorConfig = {
   appId: 'com.costantini.app',
   appName: 'abra',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    Keyboard: {
+      resize: KeyboardResize.Body,
+      style: KeyboardStyle.Dark,
+      resizeOnFullScreen: true,
+    },
+  },
 };
 
 export default config;
