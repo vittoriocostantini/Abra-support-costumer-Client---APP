a5343978359407686fe48f21440f4bfb84d7c835
victor costantini <167801447+vittoriocostantini@users.noreply.github.com>
2024-10-18 08:50:22 -0400

    None - Cambios borrados

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
+
 // Asegúrate de que el contenedor exista antes de crear el root
 const container = document.getElementById('root');
 if (container) {
     const root = createRoot(container);
     root.render(<App />);
 }
+
