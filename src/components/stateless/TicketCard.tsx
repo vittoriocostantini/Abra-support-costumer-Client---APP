da5687da8355cee51a24a0a1a67f07ce07e3776c
victor costantini <167801447+vittoriocostantini@users.noreply.github.com>
2024-10-18 09:03:58 -0400

    Mejora de navegacion => /Tickets/chat & componente basico Ioncard

--- src/components/stateless/TicketCard.tsx
+++ src/components/stateless/TicketCard.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonButton } from '@ionic/react';
+import { useHistory } from 'react-router-dom';
+
+const TicketCard: React.FC = () => {
+  const history = useHistory();
+
+  const handleCardClick = () => {
+    history.push('/Tickets/chat', { direction: 'forward' });
+  };
 
 
-function TicketCard() {
+
   return (
-    
-
-    <IonCard>
+    <IonCard onClick={handleCardClick}>
       <IonCardHeader>
         <IonCardTitle>Card Title</IonCardTitle>
         <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
       </IonCardHeader>
-      <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+
+      <IonCardContent>
+        Here's a small text description for the card content. Nothing more, nothing less.
+      </IonCardContent>
     </IonCard>
   );
 }
+
 export default TicketCard;
