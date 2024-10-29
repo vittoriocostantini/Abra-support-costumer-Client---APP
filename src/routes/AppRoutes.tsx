027facfb97b39fa544f4733966dd7ef26de51452
victor costantini <167801447+vittoriocostantini@users.noreply.github.com>
2024-10-29 16:00:14 -0400

    Modulacion de componente padre App.tsx - manejo de routing y routes path

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { IonRouterOutlet } from '@ionic/react';
 import { Route, Redirect } from 'react-router-dom';
 import Page from '../pages/Page';
-import TicketsPage from '../pages/TicketsPage';
-import ContactPage from '../pages/ContactPage';
-import SearchPage from '../pages/SearchPage';
-import SettingsPage from '../pages/SettingsPage';
-import ChatPage from '../pages/ChatPage';
+import TicketsPage from '../pages/tickets/TicketsPage';
+import ContactPage from '../pages/contact/ContactPage';
+import SearchPage from '../pages/search/SearchPage';
+import SettingsPage from '../pages/settings/SettingsPage';
+import ChatPage from '../pages/chat/ChatPage';
 
 const AppRoutes: React.FC = () => {
   return (
