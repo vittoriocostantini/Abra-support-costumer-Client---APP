26d40401e2a453adbd1c64dde4e5de5751604b00
victor costantini <167801447+vittoriocostantini@users.noreply.github.com>
2024-12-10 10:46:52 -0400

    Adaptado el redirect a /pages/tickets

--- src/services/routes/AppRoutes.tsx
+++ src/services/routes/AppRoutes.tsx
@@ -6,13 +6,12 @@ import TicketsPage from '../../pages/tickets/TicketsPage';
 import ContactPage from '../../pages/contact/ContactPage';
 import SearchPage from '../../pages/search/SearchPage';
 import SettingsPage from '../../pages/settings/SettingsPage';
-import ChatPage from '../../pages/chat/ChatPage';
+import ChatPage from '../chat/ChatPage';
 
 const AppRoutes: React.FC = () => {
   return (
     <IonRouterOutlet>
-      <Route path="/page/:name" component={Page} exact={true} />
-      <Redirect exact path="/" to="/pages/Tickets" />
+      <Redirect exact path="*" to="/pages/Tickets" />
       <Route path="/pages/Tickets" component={TicketsPage} exact={true} />
       <Route path="/pages/Contact" component={ContactPage} exact={true} />
       <Route path="/pages/SearchPage" component={SearchPage} exact={true} />
