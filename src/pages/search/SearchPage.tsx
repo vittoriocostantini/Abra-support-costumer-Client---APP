42ece04ebb0278a67ea7091e6c6bd5487f09475a
victor costantini <167801447+vittoriocostantini@users.noreply.github.com>
2024-12-10 10:49:51 -0400

    Boton de menuapp (temporal)

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,57 +1,49 @@
 import React from 'react';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonSearchbar, IonPage, IonButton, IonIcon } from '@ionic/react';
-import '../../theme/TabsApp/SearchPage.css';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonPage, IonButtons, IonMenuButton } from '@ionic/react';
+import '../../theme/Page-themes/SearchPage.css';
+import SearchBarPage from './searchbar/SearchBarPage';
 
 const SearchPage: React.FC = () => {
   return (
     <IonPage>
       <IonHeader className='header-search' translucent>
-      <IonToolbar className='toolbar-search'>
+        <IonToolbar className='toolbar-search'>
+          <IonButtons slot="start">
+          <IonMenuButton></IonMenuButton>
+        </IonButtons>
           <IonTitle className='title-search'>Busca tu caso</IonTitle>
-          </IonToolbar>
-    </IonHeader>
-    <IonContent fullscreen>
-      <IonHeader collapse='condense' className='header-case'>
-        <IonToolbar>
-          <IonTitle size='large'>Busca tu caso</IonTitle>
-
-          
         </IonToolbar>
-        <IonSearchbar 
-          animated={true} 
-          placeholder="Buscar"
-          className='searchbar'
-          ></IonSearchbar>
       </IonHeader>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem eligendi, 
-        sint non impedit laborum cupiditate perspiciatis at amet! Veniam accusantium 
-        esse voluptate consequatur unde corporis nostrum illum in odit qui.</p>
-
+      <IonContent fullscreen>
+        <IonHeader collapse='condense' className='header-case'>
+          <IonToolbar>
+            <IonTitle size='large'>Busca tu caso</IonTitle>
+          </IonToolbar>
+          <SearchBarPage />
+        </IonHeader>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem eligendi, 
-        sint non impedit laborum cupiditate perspiciatis at amet! Veniam accusantium 
-        esse voluptate consequatur unde corporis nostrum illum in odit qui.</p>
-
+          sint non impedit laborum cupiditate perspiciatis at amet! Veniam accusantium 
+          esse voluptate consequatur unde corporis nostrum illum in odit qui.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem eligendi, 
-        sint non impedit laborum cupiditate perspiciatis at amet! Veniam accusantium 
-        esse voluptate consequatur unde corporis nostrum illum in odit qui.</p>
-
+          sint non impedit laborum cupiditate perspiciatis at amet! Veniam accusantium 
+          esse voluptate consequatur unde corporis nostrum illum in odit qui.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem eligendi, 
-        sint non impedit laborum cupiditate perspiciatis at amet! Veniam accusantium 
-        esse voluptate consequatur unde corporis nostrum illum in odit qui.</p>
-
+          sint non impedit laborum cupiditate perspiciatis at amet! Veniam accusantium 
+          esse voluptate consequatur unde corporis nostrum illum in odit qui.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem eligendi, 
-        sint non impedit laborum cupiditate perspiciatis at amet! Veniam accusantium 
-        esse voluptate consequatur unde corporis nostrum illum in odit qui.</p>
-
+          sint non impedit laborum cupiditate perspiciatis at amet! Veniam accusantium 
+          esse voluptate consequatur unde corporis nostrum illum in odit qui.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem eligendi, 
-        sint non impedit laborum cupiditate perspiciatis at amet! Veniam accusantium 
-        esse voluptate consequatur unde corporis nostrum illum in odit qui.</p>
-
+          sint non impedit laborum cupiditate perspiciatis at amet! Veniam accusantium 
+          esse voluptate consequatur unde corporis nostrum illum in odit qui.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem eligendi, 
-        sint non impedit laborum cupiditate perspiciatis at amet! Veniam accusantium 
-        esse voluptate consequatur unde corporis nostrum illum in odit qui.</p>
-    </IonContent>
-  </IonPage>
+          sint non impedit laborum cupiditate perspiciatis at amet! Veniam accusantium 
+          esse voluptate consequatur unde corporis nostrum illum in odit qui.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem eligendi, 
+          sint non impedit laborum cupiditate perspiciatis at amet! Veniam accusantium 
+          esse voluptate consequatur unde corporis nostrum illum in odit qui.</p>
+      </IonContent>
+    </IonPage>
   );
 }
 
