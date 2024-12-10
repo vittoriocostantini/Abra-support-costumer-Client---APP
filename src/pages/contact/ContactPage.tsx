7203266a01b63cf83a19c5f326abfbc657cfc955
victor costantini <167801447+vittoriocostantini@users.noreply.github.com>
2024-12-10 10:50:13 -0400

    Boton de menuapp (temporal)

--- src/pages/contact/ContactPage.tsx
+++ src/pages/contact/ContactPage.tsx
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonPage } from '@ionic/react'; // Añadir IonIcon
-import '../../theme/TabsApp/ContactPage.css';
-import AgentHistory from '../../components/AgentHistory';
+import React from 'react';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonPage, IonButtons, IonMenuButton } from '@ionic/react'; // Añadir IonIcon
+import '../../theme/Page-themes/ContactPage.css';
+import AgentHistory from '../../components/stateful/agenthistory/AgentHistory';
+import AgentList from '../../components/stateful/agenthistory/agentList/AgentList';
 
 const ContactPage: React.FC = () => {
-  const [isSearchBarVisible, setIsSearchBarVisible] = useState(false); // Nuevo estado
-
   return (
     <IonPage>
       <IonHeader className='header-home' translucent>
         <IonToolbar className='toolbar-home'>
-          {!isSearchBarVisible && ( // Mostrar IonTitle solo si la barra de búsqueda no está visible
-            <IonTitle className='title-contact'>Contacta a un agente</IonTitle>
-          )}
-          <AgentHistory onToggleSearchBar={setIsSearchBarVisible} /> {/* Pasar la función de callback */}
+          <IonButtons slot="start">
+          <IonMenuButton></IonMenuButton>
+        </IonButtons>
+          <IonTitle className='title-contact'>Contacta a un agente</IonTitle>
+          <AgentHistory />
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -22,6 +22,7 @@ const ContactPage: React.FC = () => {
             <IonTitle size="large">Contacta a un agente</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <AgentList />
       </IonContent>
     </IonPage>
   );
