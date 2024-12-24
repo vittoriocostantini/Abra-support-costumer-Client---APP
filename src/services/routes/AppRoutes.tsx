76f20d700f9e4d3b3438f0d727cc9df61f157bd2
briceno jose <briceno@MacBook-Air-de-Victor.local>
2024-12-24 17:29:42 -0400

    cambio de default route = Submitcase

--- src/services/routes/AppRoutes.tsx
+++ src/services/routes/AppRoutes.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import { IonRouterOutlet } from '@ionic/react';
 import { Route, Redirect } from 'react-router-dom';
-import Page from '../../pages/Page';
 import TicketsPage from '../../pages/tickets/TicketsPage';
 import ContactPage from '../../pages/contact/ContactPage';
-import SearchPage from '../../pages/search/SearchPage';
+import SubmitCase from '../../pages/submit/SubmitCase';
 import SettingsPage from '../../pages/settings/SettingsPage';
 import ChatPage from '../chat/ChatPage';
 
 const AppRoutes: React.FC = () => {
   return (
     <IonRouterOutlet>
-      <Redirect exact path="*" to="/pages/Tickets" />
+      <Redirect exact path="/" to="/pages/SubmitCase" />
       <Route path="/pages/Tickets" component={TicketsPage} exact={true} />
       <Route path="/pages/Contact" component={ContactPage} exact={true} />
-      <Route path="/pages/SearchPage" component={SearchPage} exact={true} />
+      <Route path="/pages/SubmitCase" component={SubmitCase} exact={true} />
       <Route path="/pages/SettingsPage" component={SettingsPage} exact={true} />
       <Route path="/Tickets/chat" component={ChatPage} exact={true} />
     </IonRouterOutlet>
