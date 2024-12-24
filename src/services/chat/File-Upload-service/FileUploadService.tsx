7077ac51e5d6411c9f52237e2a89550951ec4ffe
briceno jose <briceno@MacBook-Air-de-Victor.local>
2024-12-24 17:50:41 -0400

    logica de chatpage = Upload botton chat

---
+++ src/services/chat/File-Upload-service/FileUploadService.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { IonButton, IonIcon } from '@ionic/react';
+import { add } from 'ionicons/icons';
+
+interface FileUploadButtonProps {
+    onFilesSelected: (files: FileList) => void;
+    handleButtonClick: (event: React.MouseEvent<HTMLIonButtonElement>) => void;
+}
+
+const FileUploadButton: React.FC<FileUploadButtonProps> = ({ onFilesSelected, handleButtonClick }) => {
+    const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const files = event.target.files;
+        if (files && files.length > 0) {
+            onFilesSelected(files);
+        }
+    };
+
+    return (
+        <>
+            <IonButton fill="clear" className="chat-icon-add" 
+                onClick={(event) => {
+                    handleButtonClick(event);
+                    document.getElementById('fileInput')?.click();
+                }}>
+                <IonIcon icon={add} size="large"/>
+            </IonButton>
+            <input
+                type="file"
+                accept="image/*"
+                id="fileInput"
+                style={{ display: 'none' }}
+                onChange={handleFileUpload}
+                multiple
+            />
+        </>
+    );
+};
+
+export default FileUploadButton;
