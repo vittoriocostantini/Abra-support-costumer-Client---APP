daffae3befb3d2ddcebabdb214a5b9ae155ae3c7
briceno jose <briceno@MacBook-Air-de-Victor.local>
2024-12-24 17:41:40 -0400

    mejora del componente & optimizacion, cambio de UI

--- src/components/stateful/filteroption/FilterOption.tsx
+++ src/components/stateful/filteroption/FilterOption.tsx
@@ -1,60 +1,26 @@
 import React, { useState, useRef } from 'react';
-import { IonIcon } from '@ionic/react';
-import { chevronDownOutline } from 'ionicons/icons';
 import './FilterOption.css'; // Asegúrate de tener este archivo CSS
-import { IonModal, IonContent, IonList, IonItem, IonLabel } from '@ionic/react';
+import { 
+    IonSelect, 
+    IonSelectOption 
+} from '@ionic/react';
 
 const FilterOption: React.FC = () => {
-    const modal = useRef<HTMLIonModalElement>(null);
-    const [isModalOpen, setIsModalOpen] = useState(false); // Estado para controlar la visibilidad del modal
-
-    const handleModalOpen = () => {
-        setIsModalOpen(true);
-        modal.current?.present();
-    };
-
-    const handleModalClose = () => {
-        setIsModalOpen(false);
-    };
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <div 
-        className="filter-option">
-            <h1 
-            className="container" 
-            onClick={handleModalOpen}>
-                Filtrar tus tickets
-                <IonIcon 
-                    icon={chevronDownOutline} 
-                    className={isModalOpen ? 'icon-rotated open' : 'icon-rotated'} // Cambia la clase según el estado
-                />
-            </h1>
-
-            
-                <IonContent className='ion-padding'>
-                <IonModal 
-                ref={modal} 
-                trigger="open-modal" 
-                initialBreakpoint={1} 
-                breakpoints={[0, 1]}
-                onDidDismiss={handleModalClose} // Asegúrate de cerrar el modal
-                className='ion-modal-height'
-                >
-            
-            <IonList className='ion-item' lines="none">
-                <IonItem button detail={false} onClick={() => {/* Acción para 'Enviados' */}}>
-                    <IonLabel>Enviados</IonLabel>
-                </IonItem>
-                <IonItem button detail={false} onClick={() => {/* Acción para 'En progreso' */}}>
-                    <IonLabel>En progreso</IonLabel>
-                </IonItem>
-                <IonItem button detail={false} onClick={() => {/* Acción para 'Resueltos' */}}>
-                    <IonLabel>Resueltos</IonLabel>
-                </IonItem>
-            </IonList>
-                </IonModal>
-            </IonContent>
-            
+        <div className="filter-option" >
+            <IonSelect 
+                interface="action-sheet" 
+                placeholder="Filtrar tus tickets"
+                onIonChange={(e) => console.log('Selected:', e.detail.value)}
+                className='filter-option'
+                
+            >
+                <IonSelectOption value="completado">Completado</IonSelectOption>
+                <IonSelectOption value="en_proceso">En proceso</IonSelectOption>
+                <IonSelectOption value="pendiente">Pendiente</IonSelectOption>
+            </IonSelect>
         </div>
     );
 };
