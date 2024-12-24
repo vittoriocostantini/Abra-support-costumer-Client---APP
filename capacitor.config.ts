1a6783cf710ef91ac0ee1b5a3d22e707a932469e
briceno jose <briceno@MacBook-Air-de-Victor.local>
2024-12-24 16:23:56 -0400

    cambio de permisos

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,13 +2,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 import { KeyboardResize, KeyboardStyle } from '@capacitor/keyboard';
 
 
+
 const config: CapacitorConfig = {
-  appId: 'com.costantini.app',
+  appId: 'com.costantini.abra',
   appName: 'abra',
   webDir: 'dist',
   plugins: {
     Keyboard: {
       resize: KeyboardResize.None,
+      style: KeyboardStyle.Default,
     },
   },
 };
