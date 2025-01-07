83ab8ccbbd7bf639e8be5a23e4568cb1c639e946
briceno jose <briceno@MacBook-Air-de-Victor.local>
2025-01-07 11:32:45 -0400

    Anadido logica de app & keyboard events

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-
+import { Keyboard } from "@capacitor/keyboard";
 
 // Asegúrate de que el contenedor exista antes de crear el root
 const container = document.getElementById('root');
@@ -9,4 +9,13 @@ if (container) {
     const root = createRoot(container);
     root.render(<App />);
 }
+
+// Escuchar los eventos de teclado y ajustar la altura de la aplicación
+Keyboard.addListener("keyboardWillShow", ({ keyboardHeight }) => {
+  document.querySelector("ion-app")!.style.height = `${window.outerHeight - keyboardHeight}px`;
+});
+
+Keyboard.addListener("keyboardWillHide", () => {
+  document.querySelector("ion-app")!.style.height = "";
+});
 
