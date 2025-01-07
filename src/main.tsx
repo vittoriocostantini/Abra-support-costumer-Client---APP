1ca749236d8d6f46f8e6dbc15a3996a4c61e4c24
briceno jose <briceno@MacBook-Air-de-Victor.local>
2025-01-07 13:11:32 -0400

    separacion de logica del keyboard listener

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import { Keyboard } from "@capacitor/keyboard";
 
 // Asegúrate de que el contenedor exista antes de crear el root
 const container = document.getElementById('root');
@@ -10,12 +9,5 @@ if (container) {
     root.render(<App />);
 }
 
-// Escuchar los eventos de teclado y ajustar la altura de la aplicación
-Keyboard.addListener("keyboardWillShow", ({ keyboardHeight }) => {
-  document.querySelector("ion-app")!.style.height = `${window.outerHeight - keyboardHeight}px`;
-});
-
-Keyboard.addListener("keyboardWillHide", () => {
-  document.querySelector("ion-app")!.style.height = "";
-});
+// Configurar los listeners del teclado
 
