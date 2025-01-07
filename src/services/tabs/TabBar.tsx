8cb05abf0108d25b937b4fe18a248b089bdef1b4
briceno jose <briceno@MacBook-Air-de-Victor.local>
2025-01-07 11:38:40 -0400

    anadido logica de hidetabbar & showtabbar

--- src/services/tabs/TabBar.tsx
+++ src/services/tabs/TabBar.tsx
@@ -4,15 +4,19 @@ import { personOutline,  settingsOutline, ticketOutline, addCircleOutline } from
 import './TabBar.css';
 import AppRoutes from '../routes/AppRoutes';
 import { IonRouterOutlet, IonTabs, IonTabBar, IonTabButton } from '@ionic/react';
+import { hideTabBar } from './tabbarview/Tab-Bar-View';
+
+
 
 function TabBar() {
+  hideTabBar();
   return (
     <IonRouterOutlet>
-      <IonTabs>
+      <IonTabs className='tab-bar-app' data-id='mi-tab-id'>
         <IonRouterOutlet>
           <AppRoutes />
         </IonRouterOutlet>
-        <IonTabBar slot="bottom" className='tab-bar'>
+        <IonTabBar slot="bottom" className='tab-bar' id='app-tab-bar' translucent={true}>
         <IonTabButton tab="SubmitCase" href="/pages/SubmitCase">
             <IonIcon icon={addCircleOutline} />
             <IonLabel>Caso</IonLabel>
