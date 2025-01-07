1a8d49b0bbc642ee12a7b616de700f6486956d2d
briceno jose <briceno@MacBook-Air-de-Victor.local>
2025-01-07 11:41:31 -0400

    anadido servicio de tabbarshow & tabbarview

---
+++ src/services/tabs/tabbarview/Tab-Bar-View.ts
@@ -0,0 +1,25 @@
+// Oculta el tab bar service
+export const hideTabBar = (): void => {
+    const tabBar = document.getElementById('app-tab-bar');
+    if (tabBar !== null) {
+      tabBar.style.display = 'none';
+      tabBar.style.opacity = '0';
+    }
+  };
+
+  // Muestra el tab bar service
+  
+  export const showTabBar = (): void => {
+    const tabBar = document.getElementById('app-tab-bar');
+    if (tabBar !== null) {
+      tabBar.style.opacity = '0';
+      tabBar.style.transform = 'translateY(20px)';
+      tabBar.style.display = 'flex';
+      setTimeout(() => {
+        tabBar.style.transition = 'opacity 0.2s ease, transform 0.2s ease';
+        tabBar.style.opacity = '1';
+        tabBar.style.transform = 'translateY(0)';
+      }, 10);
+    }
+  };
+  
