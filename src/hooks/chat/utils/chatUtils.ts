0328946f5d6f6e9ea2c01052f291eb54f1d34bb6
Victor Costantini <penav1058@gmail.com>
2025-01-10 13:01:15 -0400

    hook para guardar mensajes de forma local y borralos (complemento) & separacion de logica

--- src/hooks/chat/utils/chatUtils.ts
+++ src/hooks/chat/utils/chatUtils.ts
@@ -1,17 +1,50 @@
-// Este hook se encarga de enviar un mensaje y simular una respuesta automática
-export const sendMessage = (message: string, setMessages: React.Dispatch<React.SetStateAction<{ text: string; sender: string }[]>>) => {
-    if (message.trim()) {
-        setMessages(prevMessages => [...prevMessages, { text: message, sender: 'Yo' }]);
-        // Simulación de respuesta automática
-        setTimeout(() => {
-            setMessages(prevMessages => [...prevMessages, { text: 'Hola, ¿cómo puedo ayudarte?', sender: 'Bot' }]);
-        }, 1000);
-    }
-};
+import { useEffect } from 'react';
 
 // Este hook se encarga de desplazar el contenedor de mensajes hacia abajo cuando se agrega un nuevo mensaje
 export const scrollToBottom = (messagesEndRef: React.RefObject<HTMLDivElement>) => {
     if (messagesEndRef.current) {
         messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
     }
+};
+
+export const useScrollToBottom = (messagesEndRef: React.RefObject<HTMLDivElement>, messages: any[]) => {
+    useEffect(() => {
+        const scrollToBottom = ( ) => {
+            if (messagesEndRef.current) {
+                messagesEndRef.current.scrollIntoView({ behavior: "smooth"  });
+            }
+        };
+        
+
+        // Llama a scrollToBottom cada vez que los mensajes cambien
+        scrollToBottom();
+
+        // Agregar un evento de desplazamiento para detectar el fondo
+        const handleScroll = () => {
+            if (messagesEndRef.current) {
+                const { scrollTop, scrollHeight, clientHeight } = messagesEndRef.current;
+                if (scrollTop + clientHeight >= scrollHeight) {
+                    scrollToBottom();
+                }
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [messages]);
+};
+
+// Este hook se encarga de cargar los mensajes guardados en localStorage
+export const loadMessages = (chatId: string): { text: string; sender: string; chatId: string }[] => {
+    const savedMessages = localStorage.getItem(`chatMessages_${chatId}`);
+    
+    // Borrar los mensajes después de 10 segundos
+    setTimeout(() => {
+        localStorage.removeItem(`chatMessages_${chatId}`);
+    }, 10000);
+
+    return savedMessages ? JSON.parse(savedMessages) : [];
 };
