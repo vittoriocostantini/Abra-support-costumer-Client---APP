a9b04e8f701059cd8c7815e5cd7d7950089cdec0
costantini briceno <enero-2025@MacBook-Air-de-Victor.local>
2025-01-10 12:01:26 -0400

    ajustes en logica open view bottom & scrolling

--- src/services/chat/ChatPage.tsx
+++ src/services/chat/ChatPage.tsx
@@ -1,43 +1,44 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonFooter, IonButtons, IonBackButton, IonButton, IonIcon, IonAvatar } from '@ionic/react';
-import { send, person, personAddOutline } from 'ionicons/icons';
+import { send, personAddOutline } from 'ionicons/icons';
 import './ChatPage.css';
 import { handleFilesSelected } from '../../hooks/chat/FileUpload/fileHandlers';
 import ChatInput from '../../components/chat-utils/Chat-input/ChatInput';
 import FileUploadButton from '../../components/chat-utils/File-Upload-Service/FileUploadService';
-import { sendMessage, scrollToBottom } from '../../hooks/chat/utils/chatUtils';
 import MessagesList from '../../components/chat-utils/Message-Container/MessageList';
-import  '../../theme/variables.css';
+import '../../theme/variables.css';
 import { hideTabBar } from '../../services/tabs/tabbarview/Tab-Bar-View';
 import { useKeyboardListeners } from '../../hooks/chat/keyboard/keyboardHandler';
-// Componente funcional de React para la página de chat
+import { useParams } from 'react-router-dom';
+import { getAgentById } from '../../Agents/agentData';
+import { sendMessageHandler } from '../../hooks/chat/SendMessage/sendMessage';
+import { loadMessages, useScrollToBottom } from '../../hooks/chat/utils/chatUtils';
+
+
 const ChatPage: React.FC = () => {
+    const { id: chatId } = useParams<{ id: string }>();
+    const { name: agentName, avatar: agentAvatar } = getAgentById(chatId);
     const inputRef = useRef<HTMLTextAreaElement>(null);
     const [isKeyboardVisible, setIsKeyboardVisible] = useState(false);
     const [message, setMessage] = useState('');
-    const [messages, setMessages] = useState<{ text: string; sender: string }[]>([]);
+    const [messages, setMessages] = useState<{ text: string; sender: string; chatId: string }[]>(loadMessages(chatId));
     const messagesEndRef = useRef<HTMLDivElement | null>(null);
     const keyboardHeight = useRef<number>(0);
-    
 
-    // Oculta el tab bar
+    // Este hook se encarga de ocultar la barra de pestañas
     hideTabBar();
-
-    //importa logica de teclado para ajustar la altura de la aplicacion
-    
+    // Este hook se encarga de escuchar los eventos de teclado
     useKeyboardListeners();
 
-    // Usa el hook para manejar los listeners del teclado
-
-    // Efecto para detectar cambios en la visibilidad del teclado
     useEffect(() => {
         const handleKeyboardShow = (event: any) => {
-            keyboardHeight.current = event.keyboardHeight; // Asegúrate de que este valor se esté estableciendo
+            keyboardHeight.current = event.keyboardHeight;
             setIsKeyboardVisible(true);
         };
 
+        
         const handleKeyboardHide = () => {
-            keyboardHeight.current = 0; // Restablecer a 0 cuando el teclado se oculta
+            keyboardHeight.current = 0;
             setIsKeyboardVisible(false);
         };
 
@@ -50,64 +51,44 @@ const ChatPage: React.FC = () => {
         };
     }, []);
 
-    // Función para manejar el clic en los botones sin ocultar el teclado
     const handleButtonClick = (event: React.MouseEvent<HTMLIonButtonElement>) => {
         event.preventDefault();
         if (isKeyboardVisible && inputRef.current) {
             inputRef.current.focus();
         }
-        sendMessageHandler(); // Llama a sendMessage al hacer clic en el botón
+        sendMessageHandler(message, chatId, setMessages, setMessage, inputRef);
     };
 
-    const sendMessageHandler = () => {
-        sendMessage(message, setMessages); // Llama a la función importada
-        setMessage(''); // Limpiar el mensaje
-        if (inputRef.current) {
-            inputRef.current.value = ''; // Limpiar el contenido
-            inputRef.current.style.height = 'auto'; // Restablecer la altura
-        }
-    };
+    // Este hook se encarga de desplazar el contenedor de mensajes hacia abajo cuando se agrega un nuevo mensaje
+    useScrollToBottom(messagesEndRef, messages);
 
-    const scrollToBottomHandler = () => {
-        scrollToBottom(messagesEndRef); // Llama a la función importada
-
-    };
-
-    useEffect(() => {
-        scrollToBottomHandler(); // Desplazar hacia abajo cada vez que cambian los mensajes
-        
-    }, [messages]);
-
-
-
-    // Renderiza la estructura de la página de chat
     return (
         <IonPage className='chat-page'>
             <IonHeader className='header-chat'>
                 <IonToolbar className='toolbar-header'>
                     <IonButtons slot="start">
-                        <IonBackButton defaultHref="pages/Tickets" text=""/>
+                        <IonBackButton defaultHref="/pages/Tickets" text=""/>
                     </IonButtons>
                     <div className='agent-info'>
-                        <IonAvatar slot="start" className="chat-avatar">
-                            <IonIcon size='large' icon={person} />
+                        <IonAvatar slot="start" className="chat-avatar" >
+                            <img src={agentAvatar} alt="Avatar del agente"  />
                         </IonAvatar>
-                        <IonTitle className='agent-title'>Nombre del agente</IonTitle>
+                        <IonTitle className='agent-title'>{agentName}</IonTitle>
                     </div>
                     <IonButton slot="end" fill="clear" onClick={handleButtonClick}>
                         <IonIcon icon={personAddOutline} size="large"/>
                     </IonButton>
                 </IonToolbar>
             </IonHeader>
-            <IonContent id='chat-container' fullscreen scrollY={false} >
+            <IonContent id='chat-container' fullscreen scrollY={false}>
                 <MessagesList 
                     messages={messages} 
                     messagesEndRef={messagesEndRef} 
                     keyboardHeight={keyboardHeight.current}
                 />
             </IonContent>
-            <IonFooter className="chat-footer"  id='chat-footer'>
-                <IonToolbar className="toolbar-footer">
+            <IonFooter className="chat-footer" id='chat-footer' class='ion-no-border' translucent={true} >
+                <IonToolbar className="toolbar-footer" >
                     <FileUploadButton 
                         onFilesSelected={handleFilesSelected} 
                         handleButtonClick={handleButtonClick} 
@@ -126,6 +107,5 @@ const ChatPage: React.FC = () => {
     );
 };
 
-// Exporta el componente ChatPage como predeterminado
 export default ChatPage;
 
