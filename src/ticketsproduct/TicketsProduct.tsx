e880bf57db8485f50c272a664aadd8c75f228423
Victor Costantini <penav1058@gmail.com>
2025-01-10 13:03:01 -0400

    anadido ticket path id route

--- src/ticketsproduct/TicketsProduct.tsx
+++ src/ticketsproduct/TicketsProduct.tsx
@@ -5,23 +5,50 @@ const TicketsProduct: React.FC = () => {
   // Define an array of ticket data
   const tickets = [
     {
-      title: 'Ticket 1',
-      subtitle: 'Subtítulo 1',
-      description: 'Descripción del ticket 1',
+      id: '1',
+      title: 'Correo Electronico',
+      subtitle: 'cuenta de correo',
+      description: 'Porfavor, necesito ayuda con mi cuenta de correo electrónico, mi correo no acepta el codigo de verificacion.',
       path: '/Tickets/chat/',
-      avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg',
+      avatarUrl: 'src/Assets/Agents/agent1.png',
       imageAlt: 'Avatar',
     },
     {
-      title: 'Ticket 2',
-      subtitle: 'Subtítulo 2',
-      description: 'Descripción del ticket 2',
+      id: '2',
+      title: 'Pago de Steam',
+      subtitle: 'cuenta de steam',
+      path: '/Tickets/chat/',
+      description: 'Necesito ayuda con mi cuenta de steam, tengo problemas con el pago. la tarjeta de credito no se acepta. no puedo registrar mas tarjetas.',
+      avatarUrl: 'src/Assets/Agents/agent2.png',
+      imageAlt: 'Avatar',
     },
     {
-      title: 'Ticket 3',
-      subtitle: 'Subtítulo 3',
-      description: 'Descripción del ticket 3 lorem ipsum dolor sit amet,consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      id: '3',
+      title: 'Pago de Paypal',
+      subtitle: 'cuenta de paypal',
+      description: 'Necesito ayuda con mi cuenta de paypal, tengo problemas con el pago . hace 2 dias que no puedo pagar. el pago no se refleja en mi cuenta.',
+      path: '/Tickets/chat/',
+      avatarUrl: 'src/Assets/Agents/agent3.png',
+      imageAlt: 'Avatar',
     },
+    {
+      id: '4',
+      title: 'Compra de Amazon',
+      subtitle: 'compra de amazon',
+      path: '/Tickets/chat/',
+      description: 'Necesito ayuda con mi cuenta de amazon, tengo problemas con el pago. la tarjeta de credito no se acepta. no puedo registrar mas tarjetas.',
+      avatarUrl: 'src/Assets/Agents/agent4.png',
+      imageAlt: 'Avatar',
+    },
+    {
+      id: '5',
+      title: 'Registro de Fedex',
+      subtitle: 'registro de fedex',
+      path: '/Tickets/chat/',
+      description: 'Necesito ayuda con mi cuenta de fedex, tengo problemas con el pago. la tarjeta de credito no se acepta. no puedo registrar mas tarjetas.',
+      avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg',
+      imageAlt: 'Avatar',
+    }
   ];
 
   return (
@@ -29,13 +56,13 @@ const TicketsProduct: React.FC = () => {
       {tickets.map((ticket, index) => (
         <TicketCard
           key={index}
+          id={ticket.id}
           title={ticket.title}
           subtitle={ticket.subtitle || ''}
           description={ticket.description || ''}
           path={ticket.path || ''}
           avatarUrl={ticket.avatarUrl || ''}
           imageAlt={ticket.imageAlt || ''}
-    
         />
       ))}
     </div>
