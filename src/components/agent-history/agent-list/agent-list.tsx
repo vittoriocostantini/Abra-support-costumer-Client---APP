94a0c5452982681d133a64fec4ef4076265b4899
Victor Costantini <penav1058@gmail.com>
2025-01-26 11:55:20 -0400

    none

--- src/components/agent-history/agent-list/agent-list.tsx
+++ src/components/agent-history/agent-list/agent-list.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { IonList } from '@ionic/react';
-import AgentItem from './agentItem/agent-item';
+import AgentItem from './agent-item/agent-item';
 import '../../../theme/components-themes/agent-list.css';
 
 function AgentList() {
@@ -12,36 +12,11 @@ function AgentList() {
     { name: 'Cortana', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
     { name: 'Bender', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
     { name: 'BB-8', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'BB-9E', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'Rick Astley', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'Leeroy Jenkins', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'Ionitron', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'Wall-E', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'Cortana', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'Bender', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'BB-8', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'BB-9E', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'Rick Astley', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'Leeroy Jenkins', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'Ionitron', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'Wall-E', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'Cortana', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'Bender', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'BB-8', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'BB-9E', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'Rick Astley', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'Leeroy Jenkins', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'Ionitron', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'Wall-E', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'Cortana', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'Bender', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'BB-8', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'BB-9E', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
   ];
 
   return (
     <>
-        <IonList inset={true}>
+        <IonList inset={true} className='agent-list-item'>
           {agents.map((agent, index) => (
             <AgentItem key={index} name={agent.name} avatarUrl={agent.avatarUrl} />
           ))}
