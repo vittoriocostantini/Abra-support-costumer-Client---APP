7e8f82505bcfd4d4b5d64033903ff950683dc226
Victor Costantini <penav1058@gmail.com>
2025-01-26 12:06:07 -0400

    none

--- src/services/routes/app-routes.tsx
+++ src/services/routes/app-routes.tsx
@@ -6,7 +6,9 @@ import ContactPage from '../../pages/contact/contact-page';
 import SubmitCase from '../../pages/submit/submit-case';
 import SettingsPage from '../../pages/settings/settings-page';
 import ChatPage from '../chat/chat-page';
-
+import Privacity from '../../pages/settings/settings-sub-pages/privacity/privacity';
+import Languaje from '../../pages/settings/settings-sub-pages/lang/languaje';
+import AboutUs from '../../pages/settings/settings-sub-pages/about/about-us';
 
 const AppRoutes: React.FC = () => {
   return (
@@ -17,6 +19,9 @@ const AppRoutes: React.FC = () => {
       <Route path="/pages/SubmitCase" component={SubmitCase} exact={true} />
       <Route path="/pages/SettingsPage" component={SettingsPage} exact={true} />
       <Route path="/Tickets/chat/:id" component={ChatPage} exact={true} />
+      <Route path="/sub-pages/Privacity" component={Privacity} exact={true} />
+      <Route path="/sub-pages/Languaje" component={Languaje} exact={true} />
+      <Route path="/sub-pages/AboutUs" component={AboutUs} exact={true} />
     </IonRouterOutlet>
   );
 };
