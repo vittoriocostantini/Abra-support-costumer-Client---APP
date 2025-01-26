4bff01aef65d8eb0758a97661789ef6fa55815fc
Victor Costantini <penav1058@gmail.com>
2025-01-26 12:00:19 -0400

    ajuste de eleccion de icono & color

--- src/components/platform-scroll-apps/Platform-scroll-submit.tsx
+++ src/components/platform-scroll-apps/Platform-scroll-submit.tsx
@@ -8,19 +8,21 @@ const PlatformScrollSubmit: React.FC = () => {
     const [showModal, setShowModal] = useState(false);
     const [selectedApp, setSelectedApp] = useState<string>('Selecciona una app');
     const [selectedIcon, setSelectedIcon] = useState<string>(apps);
+    const [selectedColor, setSelectedColor] = useState<string>('black');
 
     return (
         <IonItem>
             <IonLabel id='label-apps'>{selectedApp}</IonLabel>
             <IonItem button lines="none" detail={false} onClick={() => setShowModal(true)}>
-                <IonIcon size="large" slot="" icon={selectedIcon} className='icon-apps' id='icon-apps'></IonIcon>
+                <IonIcon size="large" slot="" icon={selectedIcon} className='icon-apps' id='icon-apps' style={{ color: selectedColor }}></IonIcon>
             </IonItem>
             <ModalApps 
                 isOpen={showModal} 
                 onDidDismiss={() => setShowModal(false)}
-                onSelectApp={(appLabel, appIcon) => {
+                onSelectApp={(appLabel, appIcon, appColor) => {
                     setSelectedApp(appLabel);
                     setSelectedIcon(appIcon);
+                    setSelectedColor(appColor);
                 }}
             />
         </IonItem>
