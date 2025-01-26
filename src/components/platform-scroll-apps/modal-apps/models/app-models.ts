53a957e251fec6df622751c21e40494957a52e9f
Victor Costantini <penav1058@gmail.com>
2025-01-26 12:02:12 -0400

    color para iconos

--- src/components/platform-scroll-apps/modal-apps/models/app-models.ts
+++ src/components/platform-scroll-apps/modal-apps/models/app-models.ts
@@ -26,30 +26,31 @@ import { logoLinkedin,
 export interface AppItem {
     label: string;
     icon: string;
-  }
+    color: string;
+}
   
-  // Lista de aplicaciones con sus respectivos iconos
-  export const appItems: AppItem[] = [
-    { label: 'Linkedin', icon: logoLinkedin },
-    { label: 'Facebook', icon: logoFacebook },
-    { label: 'Twitter', icon: logoTwitter },
-    { label: 'Instagram', icon: logoInstagram },
-    { label: 'Pinterest', icon: logoPinterest },
-    { label: 'Snapchat', icon: logoSnapchat },
-    { label: 'Tiktok', icon: logoTiktok },
-    { label: 'Youtube', icon: logoYoutube },
-    { label: 'Reddit', icon: logoReddit },
-    { label: 'Whatsapp', icon: logoWhatsapp },
-    { label: 'Paypal', icon: logoPaypal },
-    { label: 'Amazon', icon: logoAmazon },
-    { label: 'Apple', icon: logoApple },
-    { label: 'Google', icon: logoGoogle },
-    { label: 'Microsoft', icon: logoMicrosoft },
-    { label: 'Github', icon: logoGithub },
-    { label: 'Gitlab', icon: logoGitlab },
-    { label: 'Bitbucket', icon: logoBitbucket },
-    { label: 'Docker', icon: logoDocker },
-    { label: 'Appstore', icon: logoAppleAppstore },
-  ];
+// Lista de aplicaciones con sus respectivos iconos
+export const appItems: AppItem[] = [
+    { label: 'Linkedin', icon: logoLinkedin, color: '#0077B5' },
+    { label: 'Facebook', icon: logoFacebook, color: '#3b5998' },
+    { label: 'Twitter', icon: logoTwitter, color: '#1DA1F2' },
+    { label: 'Instagram', icon: logoInstagram, color: '#C13584' },
+    { label: 'Pinterest', icon: logoPinterest, color: '#E60023' },
+    { label: 'Snapchat', icon: logoSnapchat, color: '#FFFC00' },
+    { label: 'Tiktok', icon: logoTiktok, color: '#000000' },
+    { label: 'Youtube', icon: logoYoutube, color: '#FF0000' },
+    { label: 'Reddit', icon: logoReddit, color: '#FF4500' },
+    { label: 'Whatsapp', icon: logoWhatsapp, color: '#25D366' },
+    { label: 'Paypal', icon: logoPaypal, color: '#003087' },
+    { label: 'Amazon', icon: logoAmazon, color: '#FF9900' },
+    { label: 'Apple', icon: logoApple, color: '#A2AAAD' },
+    { label: 'Google', icon: logoGoogle, color: '#4285F4' },
+    { label: 'Microsoft', icon: logoMicrosoft, color: '#F25022' },
+    { label: 'Github', icon: logoGithub, color: '#181717' },
+    { label: 'Gitlab', icon: logoGitlab, color: '#FCA121' },
+    { label: 'Bitbucket', icon: logoBitbucket, color: '#0052CC' },
+    { label: 'Docker', icon: logoDocker, color: '#2496ED' },
+    { label: 'Appstore', icon: logoAppleAppstore, color: '#0D96F6' },
+];
 
 /* app-models.ts */
