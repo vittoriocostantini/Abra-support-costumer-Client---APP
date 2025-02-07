9fb486def9b8607123460fa5adec494121a141d9
victor costantini <penav1058@gmail.com>
2025-02-07 11:14:43 -0400

    eliminado agent - cambio por function para obtener agente por name

--- src/agents-data/agent-data.ts
+++ src/agents-data/agent-data.ts
@@ -1,19 +1,23 @@
-
-// Datos de los agentes
+// Base de datos de los agentes
 export const agentsData = [
-  { id: '1', name: 'Stevan Smith', avatar: 'src/Assets/Agents/agent1.png' },
-  { id: '2', name: 'John Doe', avatar: 'src/Assets/Agents/agent2.png' },
-  { id: '3', name: 'Maria Feng', avatar: 'src/Assets/Agents/agent3.png' },
-  { id: '4', name: 'Jane Doe', avatar: 'src/Assets/Agents/agent4.png' },
+  { name: 'Stevan Smith', avatar: 'src/Assets/Agents/agent1.png' },
+  { name: 'John Doe', avatar: 'src/Assets/Agents/agent2.png' },
+  { name: 'Maria Feng', avatar: 'src/Assets/Agents/agent3.png' },
+  { name: 'Jane Doe', avatar: 'src/Assets/Agents/agent4.png' },
 ];
 
-// Función para obtener los datos del agente por su ID
-export const getAgentById = (chatId: string) => {
-    const agent = agentsData.find(agent => agent.id === chatId);
+// Función para obtener los datos del agente por su nombre
+export const getAgentByName = (agentName: string) => {
+    const agent = agentsData.find(agent => agent.name === agentName);
     return {
         name: agent ? agent.name : 'Nombre del agente',
         avatar: agent ? agent.avatar : 'https://ionicframework.com/docs/img/demos/avatar.svg'
     };
 };
 
-/* agent-data.ts */
+// Función para obtener un agente aleatorio
+export const getRandomAgent = () => {
+    const randomIndex = Math.floor(Math.random() * agentsData.length);
+    return agentsData[randomIndex];
+};
+
