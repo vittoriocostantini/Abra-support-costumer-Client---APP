925be911266a27355cb47a74f1655f39dbfb7eb5
victor costantini <penav1058@gmail.com>
2025-02-19 18:04:06 -0400

    separacion de ionitem de agent-history y agent-contant-page

--- src/components/agent-history/agent-list/agent-list.tsx
+++ src/components/agent-history/agent-list/agent-list.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import { IonList } from '@ionic/react';
-import AgentItem from './agent-item/agent-item';
+import { IonList, IonItem, IonAvatar, IonLabel, IonItemSliding } from '@ionic/react';
 import '../../../theme/components-themes/agent-list.css';
 
-function AgentList() {
-  const agents = [
-    { name: 'Rick Astley', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'Leeroy Jenkins', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'Ionitron', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'Wall-E', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'Cortana', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'Bender', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-    { name: 'BB-8', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
-  ];
+interface Agent {
+  id: number;
+  name: string;
+  role: string;
+  avatarUrl: string;
+}
 
+const agents: Agent[] = [
+  { id: 1, name: 'Rick Astley', role: 'Agent', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
+  { id: 2, name: 'Leeroy Jenkins', role: 'Agent', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
+  { id: 3, name: 'Ionitron', role: 'Agent', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
+  { id: 4, name: 'Wall-E', role: 'Agent', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
+  { id: 5, name: 'Cortana', role: 'Agent', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
+  { id: 6, name: 'Bender', role: 'Agent', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
+  { id: 7, name: 'BB-8', role: 'Agent', avatarUrl: 'https://ionicframework.com/docs/img/demos/avatar.svg' },
+];
+
+function AgentList() {
   return (
     <>
-        <IonList inset={true} className='agent-list-item'>
-          {agents.map((agent, index) => (
-            <AgentItem key={index} name={agent.name} avatarUrl={agent.avatarUrl} />
-          ))}
-        </IonList>
+      <IonList inset={true} className='agent-list'>
+        {agents.map(agent => (
+          <IonItemSliding key={agent.id}>
+            <IonItem button detail={false}> 
+              <IonAvatar slot="start">
+                <img src={agent.avatarUrl} alt="Avatar" />
+              </IonAvatar>
+              <IonLabel>
+                <h2>{agent.name}</h2>
+                <p>{agent.role}</p>
+              </IonLabel>
+            </IonItem>
+          </IonItemSliding>
+        ))}
+      </IonList>
     </>
   );
 }
