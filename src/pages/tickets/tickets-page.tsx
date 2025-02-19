97d01f005a607e6d1d04f2143e75eebedb98ff5f
victor costantini <penav1058@gmail.com>
2025-02-19 18:09:02 -0400

    implemetancion de apartado de archived-tickets & mejora de interfaz

--- src/pages/tickets/tickets-page.tsx
+++ src/pages/tickets/tickets-page.tsx
@@ -1,13 +1,24 @@
 // Importaciones de React y componentes de Ionic
 import React, { useState, useEffect } from 'react';
-import { IonContent, IonHeader, IonToolbar, IonPage, IonTitle, IonButtons, IonMenuButton, IonList } from '@ionic/react';
+import { IonContent, 
+  IonHeader, 
+  IonToolbar, 
+  IonPage, 
+  IonTitle, 
+  IonList, 
+  IonIcon, 
+  IonItem,
+  IonLabel,
+  IonRouterLink
+} from '@ionic/react';
 import '../../theme/page-themes/ticket-page.css';
 import FilterOption from '../../components/filter-option/filter-option';
 import TicketCard from '../../components/ticket-card/ticket-card';
 import { showTabBar } from '../../services/tabs/tab-bar-view/tabbar-view';
 import { useIonViewDidEnter } from '@ionic/react';
 import { tickets } from '../../tickets-store/tickets-store';
-import { loadMessages } from '../../hooks/chat/utils/chat-utils';
+import { loadMessages } from '../../hooks/chat/storage-load-messages/storage-load-messages';
+import { archiveOutline } from 'ionicons/icons';
 
 // Componente principal de la página de tickets
 const TicketsPage: React.FC = () => {
@@ -36,15 +47,32 @@ const TicketsPage: React.FC = () => {
   }, []);
 
   // Renderizado del componente
+  
   return (
-    <IonPage>
-      <IonHeader className='header-tickets' translucent >
+    <IonPage> 
+      
+      <IonHeader className='header-tickets'  class='ion-no-border' >
         <IonToolbar className='toolbar-tickets'>
+        <IonTitle>Chats</IonTitle>
           <FilterOption />
         </IonToolbar>
       </IonHeader>
+      
       <IonContent fullscreen className='content-tickets'>
-        <IonList>
+        <IonHeader collapse="condense" > 
+          <IonToolbar>
+            <IonTitle size='large' >Chats</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonList> 
+          <IonRouterLink routerLink="/tickets/sub-pages/ArchivedTickets">
+        <IonItem button detail={false} className='archived-tickets'>
+          <IonIcon size='small' icon={archiveOutline}/>
+          <IonLabel className='archive-label'>Archivados</IonLabel>
+          <p>3</p>         {/* implementar logica de conteo de tickets archivados*/}
+        </IonItem>
+        </IonRouterLink>
+      
           {updatedTickets.map((ticket, index) => {
             return (
               <TicketCard
