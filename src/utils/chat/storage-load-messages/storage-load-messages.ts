b19a5f1dbc87c6b3d9acce478f3b2aac5f4c94e9
victor costantini <penav1058@gmail.com>
2025-03-23 17:17:31 -0400

    refactor: optimizacion de logica storage utils +

--- src/utils/chat/storage-load-messages/storage-load-messages.ts
+++ src/utils/chat/storage-load-messages/storage-load-messages.ts
@@ -1,11 +1,13 @@
+import { loadMessagesFromLocalStorage } from '../storage-utils/storage-utils';
+
 // Este hook se encarga de cargar los mensajes guardados en localStorage
 export const loadMessages = (chatId: string): { text: string; sender: string; chatId: string; unread?: number }[] => {
-    const savedMessages = localStorage.getItem(`chatMessages_${chatId}`);
+    const savedMessages = loadMessagesFromLocalStorage(chatId);
     
     // Borrar los mensajes después de 10 segundos
     // setTimeout(() => {
     //     localStorage.removeItem(`chatMessages_${chatId}`);
     // }, 10000);
 
-    return savedMessages ? JSON.parse(savedMessages) : [];
+    return savedMessages;
 };
